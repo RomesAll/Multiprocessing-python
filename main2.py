4fb4dc74cd85591873858a824799ab7ecf8b34d1
Roman Beskokotov <roma@gmail.com>
2025-09-30 01:00:48 +0300

    Урок 3

---
+++ main2.py
@@ -0,0 +1,14 @@
+import multiprocessing, os, time
+CNT = 0
+
+def f():
+    global CNT 
+    print('f started from ', os.getpid())
+    time.sleep(5)
+    CNT += 2
+    print(f'f: {CNT} from ', os.getpid())
+
+if __name__ == '__main__':
+    p = multiprocessing.Process(target=f, args=())
+    p.start()
+    print('Finished')
