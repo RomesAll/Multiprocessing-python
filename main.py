961e787af529440e490e23c1d6870c3501ac46eb
Roman Beskokotov <roma@gmail.com>
2025-09-30 00:04:53 +0300

    Урок 2

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-import time
-time.sleep(100)
+import os, time
+print(f'Номер процесса {os.getpid()}')
+for i in range(2):
+    res = os.fork() # Вызов fork начиная с 4 строчки у нас кодовая база
+    time.sleep(10)
+    # начинает выполняться в 2 процессах, то есть сначало в 1 процессе выводиться
+    # на печать res а потов в другом
+    print(res)
